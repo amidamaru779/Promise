9daaa0ffd7c19f6d5584b7081c01dc2a0eafd05d
Nikita Polev <nikitko779@gmail.com>
2024-10-08 22:01:29 +0300

    выполнил задание 26

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+const get = "db.json"
+const send = "https://jsonplaceholder.typicode.com/posts"
 
+const getData = (url) => {
+    return fetch(url)
+        .then((response) => response.json())
+        .catch((err) => console.log(err))
+};
+
+const sendData = (url, data) => {
+    return fetch(url, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+        })
+        .then((response) => response.json())
+        .catch((err) => console.log(err))
+};
+
+getData(get)
+    .then((data) => {
+        return sendData(send, data)
+    })
+    .then((responseData) => {
+        console.log(responseData)
+    })
+    .catch((err) => console.log(err))
